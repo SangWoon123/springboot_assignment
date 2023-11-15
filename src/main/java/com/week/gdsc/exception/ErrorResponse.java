054a10684ede591d0fb2b932361b68adb5a296be
SangWoon123 <alrax3574@naver.com>
2023-11-15 23:19:00 +0900

    close #7 - 기능 해결(exception추가)

--- src/main/java/com/week/gdsc/exception/ErrorResponse.java
+++ src/main/java/com/week/gdsc/exception/ErrorResponse.java
@@ -2,13 +2,16 @@ package com.week.gdsc.exception;
 
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Getter
 public class ErrorResponse {
 
-    int status;
-    String errorMessage;
-
-    public ErrorResponse(int status, String errorMessage) {
+    private final LocalDateTime timestamp = LocalDateTime.now();
+    private final int status;
+    private final List<String> errorMessage;
+    public ErrorResponse(int status, List<String> errorMessage) {
         this.status = status;
         this.errorMessage = errorMessage;
     }
--- src/main/java/com/week/gdsc/exception/ExceptionController.java
+++ src/main/java/com/week/gdsc/exception/ExceptionController.java
@@ -3,7 +3,14 @@ package com.week.gdsc.exception;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 @Slf4j
@@ -13,11 +20,23 @@ public class ExceptionController {
         ErrorCode errorCode = e.getErrorCode();
 
         // ErrorResponse는 에러 응답을 나타내는 dto클래스
-        ErrorResponse errorResponse = new ErrorResponse(errorCode.getStatus(), errorCode.getMessage());
+        ErrorResponse errorResponse = new ErrorResponse(errorCode.getStatus(), Collections.singletonList(errorCode.getMessage()));
 
         log.error("handleBusinessException: "+errorCode.getMessage());
 
         return new ResponseEntity(errorResponse,HttpStatus.valueOf(errorCode.getStatus()));
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> methodValidException(MethodArgumentNotValidException e){
+        List<String> errorMessage=e.getBindingResult().getAllErrors().stream()
+                .map(ObjectError::getDefaultMessage)
+                .collect(Collectors.toList());
+        ErrorResponse errorResponse=new ErrorResponse(401,errorMessage);
+        return new ResponseEntity<ErrorResponse>(errorResponse,HttpStatus.BAD_REQUEST);
+    }
+
+
+
 
 }
